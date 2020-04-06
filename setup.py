202eb7b0e26df6f75af151564d8b8db7a91bb03a
Jake Uskoski <jake.uskoski@fiixsoftware.com>
2020-04-05 20:59:39 -0400

    Setting package url to github repo

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="A terminal-based moddable RPG",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/juskoski/",
+    url="https://github.com/JakeUskoski/dreadlight",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
