af47c199b6847cd843ec515eb93701859b48a20e
Jake Uskoski <jake.uskoski@fiixsoftware.com>
2020-04-10 17:21:03 -0400

    Fix root directory path builder

--- dreadlight/data/paths.py
+++ dreadlight/data/paths.py
@@ -1,7 +1,7 @@
 import os
 from pathlib import Path
 
-ROOT_DIR = os.path.join(Path.home().absolute().resolve().as_uri(), '.dreadlight')
+ROOT_DIR = os.path.join(str(Path.home().absolute().resolve()), '.dreadlight')
 SAVE_DIR = os.path.join(ROOT_DIR, 'saves')
 SHOPS_DIR = os.path.join(SAVE_DIR, 'shops')
 ENEMIES_DIR = os.path.join(ROOT_DIR, 'enemies')
@@ -17,23 +17,16 @@ LOCATIONS_DIR = os.path.join(ROOT_DIR, 'locations')
 DISCOVERIES_DIR = os.path.join(SAVE_DIR, 'discoveries')
 
 
-def make_path(path):
-    if not os.path.exists(path):
-        os.mkdir(path)
+def make_paths(*paths):
+    for path in paths:
+        if not os.path.exists(path):
+            os.mkdir(path)
 
 
 def prepare_paths():
-    make_path(ROOT_DIR)
-    make_path(SAVE_DIR)
-    make_path(SHOPS_DIR)
-    make_path(ENEMIES_DIR)
-    make_path(ITEMS_DIR)
-    make_path(MAGIC_DIR)
-    make_path(SKILLS_DIR)
-    make_path(CLASSES_DIR)
-    make_path(STATS_DIR)
-    make_path(ATTRIBUTES_DIR)
-    make_path(TYPES_DIR)
-    make_path(CONCEPTS_DIR)
-    make_path(LOCATIONS_DIR)
-    make_path(DISCOVERIES_DIR)
+    make_paths(ROOT_DIR)
+    make_paths(TYPES_DIR, ATTRIBUTES_DIR, STATS_DIR)
+    make_paths(LOCATIONS_DIR, CONCEPTS_DIR)
+    make_paths(ITEMS_DIR, MAGIC_DIR, SKILLS_DIR)
+    make_paths(SAVE_DIR, SHOPS_DIR, DISCOVERIES_DIR)
+    make_paths(CLASSES_DIR, ENEMIES_DIR)
